920192bca32aa5f1ecc1fb49f820289f499930f9
Anshika1507 <98647659+Anshika1507@users.noreply.github.com>
2022-01-29 19:35:26 +0530

    Add files via upload
    Print the table of i/p number

---
+++ Tables.java
@@ -0,0 +1,15 @@
+package com.example.tables;
+import java.util.Scanner;
+
+public class Tables {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for which you want to print table:");
+        int num = sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println(num + " * "+i + " ="+(num*i));
+        }
+    }
+}
