f3e3878f94e1d2934eb1b3b575d472051a080719
Anshika1507 <98647659+Anshika1507@users.noreply.github.com>
2022-01-29 19:47:15 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+package com.example.forloop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int i=0;
+        for(; i< 5; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("value of i:"+i);
+
+    }
+}
